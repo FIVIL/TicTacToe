c2490f2f396ddab254ce9c5324e49ac2cf57bdc3
FIVIL <hammedmohamadi@gmail.com>
2024-05-12 16:23:10 -0400

    Game should work

--- test/TicTacToe.t.sol
+++ test/TicTacToe.t.sol
@@ -1,1 +1,10 @@
 
+//// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TicTacToe} from "../src/TicTacToe.sol";
+
+contract CounterTest is Test {
+
+}
